c4987ec7f50056b20fe7adc11f388d917b210d01
AhmedSabry13 <40600333+AhmedSabry13@users.noreply.github.com>
2025-03-25 22:39:50 +0200

    Delete modules directory

--- modules/eks/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "cluster_id" {
-  description = "The EKS cluster ID"
-  value       = aws_eks_cluster.eks_cluster.id
-}
-
-output "cluster_endpoint" {
-  description = "The EKS cluster API endpoint"
-  value       = aws_eks_cluster.eks_cluster.endpoint
-}
