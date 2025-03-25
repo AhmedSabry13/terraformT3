29e56f3a60ac6a52ad4261c715b512bb2d8b4deb
AhmedSabry13 <ahmednaruto13@gmail.com>
2025-03-25 22:46:51 +0200

    'firstfa'

--- main.tf
+++ main.tf
@@ -34,14 +34,30 @@ data "aws_region" "current" {}
 data "aws_availability_zones" "available" {}
 
 resource "aws_instance" "apache_ec2" {
-  ami                    = var.ami_image  # Replace with an appropriate AMI ID for your region
+  ami                    = "ami-08f9a9c699d2ab3f9"  # Replace with an appropriate AMI ID for your region
   instance_type          = "t2.micro"     # Adjust instance type as needed
   key_name               = "sab"          # Replace with your key pair name
   associate_public_ip_address = true      # Public IP
   vpc_security_group_ids = [aws_security_group.web_sg.id]  # Corrected security group reference
   availability_zone      = data.aws_availability_zones.available.names[0]
 
-  user_data = var.user_data_apache
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo dnf install -y httpd
+
+              sudo systemctl start httpd
+              sudo systemctl enable httpd
+
+              sudo chown ec2-user: /var/www/html
+
+              # Get the private IP address of the EC2 instance
+              PRIVATE_IP=$(hostname -I | awk '{print $1}')
+
+              # Create the index.html file with the private IP displayed
+              echo "<h1>Hello from Sabry 1 terraform - Private IP: $PRIVATE_IP</h1>" > /var/www/html/index.html
+
+              sudo systemctl restart httpd
+              EOF
 
   tags = {
     Name      = "demo_sabry_2"
