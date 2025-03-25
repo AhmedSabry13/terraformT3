9295a8c199fdcefa73da7e29bac53c7b24e0b408
AhmedSabry13 <ahmednaruto13@gmail.com>
2025-03-25 23:15:14 +0200

    'firstfadd'

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ data "aws_region" "current" {}
 # Fetch the first available availability zone in the region
 data "aws_availability_zones" "available" {}
 
-resource "aws_instance" "apache_ec2" {
+resource "aws_instance" "apache_ec2_yyy" {
   ami                    = "ami-08f9a9c699d2ab3f9"  # Replace with an appropriate AMI ID for your region
   instance_type          = "t2.micro"     # Adjust instance type as needed
   key_name               = "sab"          # Replace with your key pair name
@@ -54,7 +54,7 @@ resource "aws_instance" "apache_ec2" {
               PRIVATE_IP=$(hostname -I | awk '{print $1}')
 
               # Create the index.html file with the private IP displayed
-              echo "<h1>Hello from Sabry 1 terraform - Private IP: $PRIVATE_IP</h1>" > /var/www/html/index.html
+              echo "<h1>Hello from Sabry 1 terraform jenkins- Private IP: $PRIVATE_IP</h1>" > /var/www/html/index.html
 
               sudo systemctl restart httpd
               EOF
